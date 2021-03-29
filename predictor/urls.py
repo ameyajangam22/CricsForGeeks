d00fe6bd22764fa660ec680519527f46fa9dd893
Ameya <ameya.g.jangam@gmail.com>
2021-03-29 16:16:43 +0530

    Added team selection and team option

--- predictor/urls.py
+++ predictor/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-
+    path("teamchoice/", views.teamchoice, name="teamchoice"),
+    path("teamoption/", views.teamoption, name="teamoption"),
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
 from predictor.models import *
 # from predictor.forms import *
-
 from django.forms import inlineformset_factory
 # from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
@@ -16,3 +16,18 @@ from django.contrib.auth.models import Group
 
 def home(request):
     return render(request, 'predictor/home.html')
+
+
+def teamchoice(request):
+    if 'term' in request.GET:
+        qs = Player.objects.filter(
+            Player_Name__icontains=request.GET.get('term'))
+        names = list()
+        for player in qs:
+            names.append(player.Player_Name)
+        return JsonResponse(names, safe=False)
+    return render(request, 'predictor/teamchoice.html')
+
+
+def teamoption(request):
+    return render(request, 'predictor/teamoption.html')
