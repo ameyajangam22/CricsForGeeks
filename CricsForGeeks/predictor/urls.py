13780898ce8693a3722abaeb4df368f68de0065b
Ameya <ameya.g.jangam@gmail.com>
2021-05-30 16:56:30 +0530

    Added Teams vs Teams feature

--- CricsForGeeks/predictor/urls.py
+++ CricsForGeeks/predictor/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("teamchoice/", views.teamchoice, name="teamchoice"),
     path("teamoption/", views.teamoption, name="teamoption"),
+    path("teamvteam/", views.teamvteam, name="teamvteam"),
     path("rules/", views.rules, name="rules"),
     path("stats/", views.stats, name="stats"),
 
--- CricsForGeeks/predictor/views.py
+++ CricsForGeeks/predictor/views.py
@@ -19,50 +19,93 @@ def home(request):
     return render(request, 'predictor/home.html')
 
 
+def teamvteam(request):
+    return render(request, 'predictor/teamVteam.html')
+
+
 def stats(request):
     teamA = []
     teamB = []
     context = {}
     if request.method == "POST":
-        alen = int(request.POST["teamAL"])
-        blen = int(request.POST["teamBL"])
+        if request.POST["teamAL"] == 1 and request.POST["teamBL"] == 1:
 
-        for i in range(alen):
-            teamA.append(request.POST["teamA"+str(i)])
-        for i in range(blen):
-            teamB.append(request.POST["teamB"+str(i)])
+            alen = int(request.POST["teamAL"])
+            blen = int(request.POST["teamBL"])
 
-        final_batsman_a_stats, final_bowler_a_stats, final_batsman_b_stats, final_bowler_b_stats, total_a, total_b = predicter.get_prediction(
-            teamA, teamB)
+            for i in range(alen):
+                teamA = (request.POST["teamA"+str(i)])
+            for i in range(blen):
+                teamB = (request.POST["teamB"+str(i)])
 
-        context = {"bat_a": {i: final_batsman_a_stats[i] for i in sorted(final_batsman_a_stats.keys(), key=final_batsman_a_stats.get, reverse=True)},
-                   "bat_b": {i: final_batsman_b_stats[i] for i in sorted(final_batsman_b_stats.keys(), key=final_batsman_b_stats.get, reverse=True)},
-                   "bowl_a": {i: [final_bowler_a_stats[i][0], final_bowler_a_stats[i][1]//6] for i in sorted(final_bowler_a_stats.keys(), key=final_bowler_a_stats.get, reverse=True)},
-                   "bowl_b": {i: [final_bowler_b_stats[i][0], final_bowler_b_stats[i][1]//6] for i in sorted(final_bowler_b_stats.keys(), key=final_bowler_b_stats.get, reverse=True)},
-                   "total_a": total_a,
-                   "total_b": total_b
-                   }
+            final_batsman_a_stats, final_bowler_a_stats, final_batsman_b_stats, final_bowler_b_stats, total_a, total_b = predicter.get_prediction(
+                teamA, teamB)
 
-        # print(context)
-        # print('Batsman Team A')
-        # for i in final_batsman_a_stats:
-        #     print(i, final_batsman_a_stats[i][0],
-        #           'in', final_batsman_a_stats[i][1])
-        # print('Total =', sum(i[0] for i in final_batsman_a_stats.values()))
-        # print('Bowlers Team B')
-        # for i in final_bowler_b_stats:
-        #     print(i, final_bowler_b_stats[i][0], 'conceded in',
-        #           final_bowler_b_stats[i][1]//6, 'overs')
-        # print('*'*100)
-        # print('Batsman Team B')
-        # for i in final_batsman_b_stats:
-        #     print(i, final_batsman_b_stats[i][0],
-        #           'in', final_batsman_b_stats[i][1])
-        # print('Bowlers Team A')
-        # for i in final_bowler_a_stats:
-        #     print(i, final_bowler_a_stats[i][0], 'conceded in',
-        #           final_bowler_a_stats[i][1]//6, 'overs')
-        # print('Total =', sum(i[0] for i in final_batsman_b_stats.values()))
+            context = {"bat_a": {i: final_batsman_a_stats[i] for i in sorted(final_batsman_a_stats.keys(), key=final_batsman_a_stats.get, reverse=True)},
+                       "bat_b": {i: final_batsman_b_stats[i] for i in sorted(final_batsman_b_stats.keys(), key=final_batsman_b_stats.get, reverse=True)},
+                       "bowl_a": {i: [final_bowler_a_stats[i][0], final_bowler_a_stats[i][1]//6] for i in sorted(final_bowler_a_stats.keys(), key=final_bowler_a_stats.get, reverse=True)},
+                       "bowl_b": {i: [final_bowler_b_stats[i][0], final_bowler_b_stats[i][1]//6] for i in sorted(final_bowler_b_stats.keys(), key=final_bowler_b_stats.get, reverse=True)},
+                       "total_a": total_a,
+                       "total_b": total_b,
+                       "teamA_name": "Team A",
+                       "teamB_name": "Team B"
+                       }
+        else:
+            alen = int(request.POST["teamAL"])
+            blen = int(request.POST["teamBL"])
+
+            for i in range(alen):
+                teamA_name = (request.POST["teamA"])
+            for i in range(blen):
+                teamB_name = (request.POST["teamB"])
+            MI = ['RG Sharma', 'Q de Kock', 'HH Pandya',
+                  'J Yadav', 'TA Boult', 'KH Pandya', 'JJ Bumrah', 'Ishan Kishan', 'KA Pollard', 'RD Chahar', 'SA Yadav']
+            # 5 not in playing 11
+            RCB = ['V Kohli', 'GJ Maxwell', 'AB de Villiers',
+                   'Washington Sundar', 'HV Patel', 'YS Chahal', 'Mohammed Siraj', 'DT Christian', 'KW Richardson', 'Sachin Baby', 'S Kuggeleijn']
+            RR = ['JC Buttler', 'SV Samson', 'S Dube',
+                  'DA Miller', 'R Parag', 'R Tewatia', 'CH Morris', 'S Gopal', 'Mustafizur Rahman', 'JD Unadkat', 'Kuldeep Yadav']
+            CSK = ['F du Plessis', 'SK Raina', 'M Ali',
+                   'AT Rayudu', 'RA Jadeja', 'MS Dhoni', 'S Curran', 'L Ngidi', 'DL Chahar', 'SN Thakur', 'DJ Bravo']
+
+            PBKS = ['KL Rahul', 'MA Agarwal', 'CH Gayle',
+                    'N Pooran', 'DJ Hooda', 'MC Henriques', 'Mohammed Shami', 'A Singh', 'R Bishnoi', 'CJ Jordan', 'M Ashwin']
+
+            KKR = ['N Rana', 'S Gill', 'RA Tripathi',
+                   'EJG Morgan', 'KD Karthik', 'SP Narine', 'AD Russell', 'PJ Cummins', 'V Chakravarthy', 'S Mavi', 'Shakib Al Hasan']
+            DC = ['P Shaw', 'S Dhawan', 'RR Pant', 'SPD Smith',
+                  'S Hetmyer', 'MP Stoinis', 'K Rabada', 'I Sharma', 'AR Patel', 'A Mishra', 'Avesh Khan']
+            SRH = ['DA Warner', 'J Bairstow', 'KS Williamson',
+                   'MK Pandey', 'KM Jadhav', 'V Shankar', 'Rashid Khan', 'J Suchith', 'Sandeep Sharma', 'K Ahmed', 'S Kaul  ']
+
+            teams = {"Chennai Super Kings": CSK,
+                     "Delhi Capitals": DC,
+                     "Kolkata Night Riders": KKR,
+                     "Mumbai Indians": MI,
+                     "Punjab Kings": PBKS,
+                     "Rajasthan Royals": RR,
+                     "Royal Challenges Bangalore": RCB,
+                     "Sunrisers Hyderabad": SRH}
+            final_batsman_a_stats, final_bowler_a_stats, final_batsman_b_stats, final_bowler_b_stats, total_a, total_b = predicter.get_prediction(
+                teams[teamA_name], teams[teamB_name])
+            team_short = {"Chennai Super Kings": "CSK",
+                          "Delhi Capitals": "DC",
+                          "Kolkata Night Riders": "KKR",
+                          "Mumbai Indians": "MI",
+                          "Punjab Kings": "PBKS",
+                          "Rajasthan Royals": "RR",
+                          "Royal Challenges Bangalore": "RCB",
+                          "Sunrisers Hyderabad": "SRH"}
+            context = {"bat_a": {i: final_batsman_a_stats[i] for i in sorted(final_batsman_a_stats.keys(), key=final_batsman_a_stats.get, reverse=True)},
+                       "bat_b": {i: final_batsman_b_stats[i] for i in sorted(final_batsman_b_stats.keys(), key=final_batsman_b_stats.get, reverse=True)},
+                       "bowl_a": {i: [final_bowler_a_stats[i][0], final_bowler_a_stats[i][1]//6] for i in sorted(final_bowler_a_stats.keys(), key=final_bowler_a_stats.get, reverse=True)},
+                       "bowl_b": {i: [final_bowler_b_stats[i][0], final_bowler_b_stats[i][1]//6] for i in sorted(final_bowler_b_stats.keys(), key=final_bowler_b_stats.get, reverse=True)},
+                       "total_a": total_a,
+                       "total_b": total_b,
+                       "teamA_name": team_short[teamA_name],
+                       "teamB_name": team_short[teamB_name]
+                       }
+
     return render(request, "predictor/stats.html", context)
 
 
